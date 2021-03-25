ee651af89eca3b70e8f4f34249c43c156fa8facd
Sairo Guanipa <sairo.guanipa@dfacture.com>
2021-03-25 07:06:13 -0600

    All CRUD operations added.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root "pages#home"
   get 'about', to: 'pages#about'
 
-  resources :articles, only: [:show]
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
